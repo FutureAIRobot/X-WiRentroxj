4dffdbfbc71e29ce8c91c8ef386c43e32d98b879
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2023-05-30 09:56:29 +0530

    Update start.py

--- Rentrox/plugins/start.py
+++ Rentrox/plugins/start.py
@@ -4,9 +4,6 @@ logger = logging.getLogger(__name__)
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters
-from bot import channelforward
-from config import Config
-from translation import Translation
 
 url_button=InlineKeyboardMarkup(
         [
@@ -23,7 +20,7 @@ url_button=InlineKeyboardMarkup(
 @channelforward.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message):
     await message.reply(
-        text=Translation.START,
+        text="Hi There I'm Rentrox",
         disable_web_page_preview=True,
         reply_markup = url_button,
         quote=True
@@ -35,7 +32,7 @@ async def start(client, message):
 @channelforward.on_message(filters.command("about") & filters.private & filters.incoming)
 async def about(client, message):
     await message.reply(
-        text=Translation.ABOUT,
+        text="Here is About me",
         disable_web_page_preview=True,
         reply_markup = url_button,
         quote=True
