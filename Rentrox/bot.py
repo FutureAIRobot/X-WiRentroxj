9f5f3c6d83da2ed9c360937b84a612277213a139
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2023-05-30 10:42:41 +0530

    Update bot.py

--- Rentrox/bot.py
+++ Rentrox/bot.py
@@ -1,6 +1,8 @@
-from pyrogram import Client, enums, __version__
+from pyrogram import Client, __version__
 
-from Rentrox import Config, LOGGER
+from pyrogram.types import ParseMode
+
+from Rentrox.config import Config, LOGGER
 
 from Rentrox.user import User
 
@@ -14,21 +16,17 @@ class Rentrox(Client):
 
         super().__init__(
 
-            "rentrox",
+            session_name="rentrox",
 
             api_hash=Config.API_HASH,
 
             api_id=Config.API_ID,
 
-            plugins={
-
-                "root": "plugins"
-
-            },
+            plugins={"root": "plugins"},
 
             workers=200,
 
-            bot_token=Config.BOT_TOKEN            
+            bot_token=Config.BOT_TOKEN
 
         )
 
@@ -40,11 +38,11 @@ class Rentrox(Client):
 
         bot_details = await self.get_me()
 
-        self.set_parse_mode(enums.ParseMode.HTML)
+        self.set_parse_mode(ParseMode.HTML)
 
         self.LOGGER(__name__).info(
 
-            f"@{bot_details.username}  started! "
+            f"@{bot_details.username} started! (Pyrogram {__version__})"
 
         )
 
@@ -55,3 +53,5 @@ class Rentrox(Client):
         await super().stop()
 
         self.LOGGER(__name__).info("Bot stopped. Bye.")
+
+
